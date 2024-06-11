c662ef5b56af6fe0a1c3dca4527a77ba7cadcea5
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2024-06-11 12:32:20 -0300

    creating the components docs

---
+++ packages/docs/src/components/TokensGrid.tsx
@@ -0,0 +1,32 @@
+import '../styles/tokens.grid.css'
+interface TokensGridProps {
+  tokens: Record<string, string>
+  hasRealValue?: boolean
+}
+export function TokensGrid({ tokens, hasRealValue = false }: TokensGridProps) {
+  return (
+    <table className="tokens-grid">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Value</th>
+          {hasRealValue && <th>Pixels</th>}
+        </tr>
+      </thead>
+
+      <tbody>
+        {Object.entries(tokens).map(([key, value]) => {
+          return (
+            <tr key={key}>
+              <td>{key}</td>
+              <td>{value}</td>
+              {hasRealValue && (
+                <td>{Number(value.replace('rem', '')) * 16}px</td>
+              )}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
