673e2385d115d36dcc47e47aae09c5a4ee0cd76e
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2024-06-17 12:00:55 -0300

    correcting the size typing error

--- packages/react/src/components/TextInput/index.tsx
+++ packages/react/src/components/TextInput/index.tsx
@@ -1,18 +1,15 @@
-import { Input, Prefix, TextInputContainer } from './styles'
-import { ComponentProps, forwardRef, ElementRef } from 'react'
 export interface TextInputProps extends ComponentProps<typeof Input> {
   prefix?: string
+  containerProps?: ComponentProps<typeof TextInputContainer>
 }
 
 export const TextInput = forwardRef<ElementRef<typeof Input>, TextInputProps>(
-  ({ prefix, ...props }: TextInputProps, ref) => {
+  ({ prefix, containerProps, ...props }, ref) => {
     return (
-      <TextInputContainer>
+      <TextInputContainer {...containerProps}>
         {!!prefix && <Prefix>{prefix}</Prefix>}
         <Input ref={ref} {...props} />
       </TextInputContainer>
     )
   },
 )
-
-TextInput.displayName = 'Text Input'
